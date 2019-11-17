e305954d612acfba5f31d7f933525a161a218ce5
lolor <1721753977@qq.com>
2019-11-17 14:33:08 +0800

    Initial commit

---
+++ src/com/niubi/Nb.java
@@ -0,0 +1,10 @@
+package com.niubi;
+
+public class Nb {
+
+    public static void main(String[] args) {
+        System.out.println("he is niubi");
+        System.out.println("he is niuqi");
+    }
+
+}
