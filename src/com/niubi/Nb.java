622f7c755dd6b586bb2fddbb3e94592564b88a6e
lolor <1721753977@qq.com>
2019-11-17 14:47:08 +0800

    helloniunai

--- src/com/niubi/Nb.java
+++ src/com/niubi/Nb.java
@@ -5,6 +5,7 @@ public class Nb {
     public static void main(String[] args) {
         System.out.println("he is niubi");
         System.out.println("he is niuqi");
+        System.out.println("he is niunai");
     }
 
 }
