a78b58a0bd9f2b4a796fffdd06c2bbd4484485bf
lolor <1721753977@qq.com>
2019-11-17 15:35:30 +0800

    commit-niubi

--- src/com/niubi/Nb.java
+++ src/com/niubi/Nb.java
@@ -5,7 +5,7 @@ public class Nb {
     public static void main(String[] args) {
         System.out.println("he is niubi");
         System.out.println("he is niuqi");
-        System.out.println("he is niunainai");
+        System.out.println("he is niunainai-niubi");
     }
 
 }
